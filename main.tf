1b4b7c7503947a1f4d4b6ae19c5daf9ee54d0a84
rej23 <107949594+rej23@users.noreply.github.com>
2024-02-04 10:25:12 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,8 +16,8 @@ resource "aws_instance" "example" {
      connection {
      type        = "ssh"
      user        = "ubuntu"
-     private_key = filebase64("${{ secrets.PRIVATE_KEY }}")
-     # private_key = filebase64(${{ secrets.PRIVATE_KEY }})
+     private_key = filebase64(${{ secrets.PRIVATE_KEY }})
+     # private_key = filebase64("${{ secrets.PRIVATE_KEY }}")
      host        = self.public_ip
    }
 
